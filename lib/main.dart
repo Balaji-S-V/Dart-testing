ec0b507c23452acd3b47f07539a549f4cf5215df
Balaji S V <86550781+Balaji-S-V@users.noreply.github.com>
2023-03-29 15:16:14 +0530

    ~2

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,93 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:local_auth/local_auth.dart';
 
-void main() => runAppp(MaterialApp
-(
-  debugShowCheckedModeBanner: false,
-  home: FingerprintApp()
-  ))
-
-
-class FingerprintApp extends StatefulWidget {
-  const FingerprintApp({super.key});
-
-  @override
-  State FingerprintApp> createState() =>  FingerprintAppState();
+void main() {
+  runApp(MyApp());
 }
 
-class  FingerprintAppState extends State FingerprintApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: 'Biometric Button Example',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Biometric Button Example'),
+        ),
+        body: Center(
+          child: BiometricButton(),
+        ),
+      ),
+    );
+  }
+}
+
+class BiometricButton extends StatelessWidget {
+  final LocalAuthentication _localAuthentication = LocalAuthentication();
+
+  Future<void> _authenticate(BuildContext context) async {
+    if (Platform.isIOS) {
+      // iOS supports Touch ID and Face ID
+      bool authenticated = false;
+      try {
+        authenticated = await _localAuthentication.authenticate(
+          localizedReason: 'Please authenticate to continue',
+          useErrorDialogs: true,
+          stickyAuth: true,
+        );
+      } on PlatformException catch (e) {
+        print(e);
+      }
+
+      if (authenticated) {
+        print('User authenticated');
+      } else {
+        print('Authentication failed');
+      }
+    } else if (Platform.isAndroid) {
+      // Android supports fingerprint authentication
+      try {
+        bool canCheckBiometrics = await _localAuthentication.canCheckBiometrics;
+        if (canCheckBiometrics) {
+          bool authenticated = false;
+          try {
+            authenticated = await _localAuthentication.authenticate(
+              localizedReason: 'Please authenticate to continue',
+              useErrorDialogs: true,
+              stickyAuth: true,
+            );
+          } on PlatformException catch (e) {
+            print(e);
+          }
+
+          if (authenticated) {
+            print('User authenticated');
+          } else {
+            print('Authentication failed');
+          }
+        } else {
+          // Fingerprint authentication not available on this device
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                  'Fingerprint authentication not available on this device.'),
+            ),
+          );
+        }
+      } on PlatformException catch (e) {
+        print(e);
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () => _authenticate(context),
+      child: Text('Authenticate'),
+    );
   }
 }
